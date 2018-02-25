e100df468f675b2cd2783b5d3c9dc1af389928b0
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-02-25 14:59:17 -0500

    small update to config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 require('dotenv').config()
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "s2lugpg6wdxixion"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_PASS,
+        database: 'nym_db'
+    });
+};    
 
 connection.connect(function(err) {
     if (err) {
