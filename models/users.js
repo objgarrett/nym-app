c049633cd5980f56e204eff917bf6775e8cffafc
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-02-25 11:26:41 -0500

    updated users.js

--- models/users.js
+++ models/users.js
@@ -1,45 +1,23 @@
-// //captures user sign up information
-// module.exports = function(sequelize, DataTypes) {
-//     var Users = sequelize.define("Tasklist", {
-//         id: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false
-//         },
+var orm = require("../config/orm.js");
 
-//         firstname: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
+var user = {
+  all: function(cb) {
+    orm.all("users", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("users", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("users", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-//         lastname: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-
-//         birthday: {
-//             type: DataTypes.DATE,
-//             defaultValue: false
-//         },
-
-//         city: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-
-//         state: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//         },
-
-//         zip: {
-//             type: DataTypes.INTEGER,
-//             allowNull: false
-//         },
-
-//         create_at: {
-//             type: DataTypes.DATE,
-//             allowNull: false
-//         }
-//     });
-//     return Users;
-// };
+// Export the database functions for the controller (catsController.js).
+module.exports = user;
