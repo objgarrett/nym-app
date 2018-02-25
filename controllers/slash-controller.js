7c6b380a3c75fd31fab50ab8a99ca9bd59752071
oitowl7 <shear_j@hotmail.com>
2018-02-25 17:18:49 -0500

    added slash controller, working on facebook login stuff

---
+++ controllers/slash-controller.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+const path = require("path");
+
+//import the model (users.js) to use its database functions
+var nym = require("../models/users.js");
+var login = require("./login-controller.js")
+
+//create all our routes and set up logic with those routes where required 
+//***************/
+//OBG's NOTE: Not sure if the logic in router.get is what we want...was going off the "MVC Example" in week 14 - hopefully this at least gets you started
+//***************/
+router.get("/", function(req, res) {
+    var loggedin = false;
+    if (loggedin === true){
+        console.log("home happened")
+        res.redirect("/home");
+    } else {
+        console.log("login happened")
+        res.redirect("/login");
+    }
+});
+
+router.get("/login", function(req, res) {
+    nym.all(function(data) {
+        var hbsObject = {
+            users: data
+        };
+        console.log(hbsObject);
+        res.render("login", hbsObject);
+    });
+});
+
+router.get("/home", function(req, res) {
+    var test
+    res.render("home", test);
+})
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ models/sessions.js
@@ -0,0 +1,18 @@
+var orm = require("../config/orm.js");
+
+var user = {
+  all: function(cb) {
+    orm.all("sessions", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("sessions", cols, vals, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = user;
