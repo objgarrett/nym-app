60e8034473ab2b0f6dc0ac0c5e583438862d1815
Brittany <brittanygillespie31@gmail.com>
2018-02-25 20:15:03 -0500

    messing with the payment API

--- controllers/slash-controller.js
+++ controllers/slash-controller.js
@@ -4,8 +4,8 @@ const path = require("path");
 
 //import the model (users.js) to use its database functions
 var nym = require("../models/users.js");
-var login = require("./login-controller.js")
-
+var login = require("./login-controller.js");
+var payment = require("./payment-controller.js");
 //create all our routes and set up logic with those routes where required 
 //***************/
 //OBG's NOTE: Not sure if the logic in router.get is what we want...was going off the "MVC Example" in week 14 - hopefully this at least gets you started
