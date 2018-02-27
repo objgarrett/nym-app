d675321655f021b0b53089305a8c3e197e15bb07
Mary Hall <mh6uj@students-MBP.home>
2018-02-26 20:09:26 -0500

    houseuser relationship model

--- models/households.js
+++ models/households.js
@@ -1,3 +1,39 @@
+var orm = require("../config/orm.js");
+
+var house = {
+  //This might not be necessary to select all because, it's just the id and pwd
+  all: function(cb) {
+    orm.all("house", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("house", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("house", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = house;
+
+
+
+
+
+
+
+
+
+
+
+
 // module.exports = function(sequelize, DataTypes) {
 //     var Households = sequelize.define("Households", {
 //         householdid: {
