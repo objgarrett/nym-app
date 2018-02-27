34d96e901c34b432b0192a919bdbcf303808d74a
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-02-26 21:30:51 -0500

    adding in inventory.handlebars

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: "password",
-        database: 'nym'
+        password: process.env.DB_PASS,
+        database: 'nym_db'
     });
 };    
 
