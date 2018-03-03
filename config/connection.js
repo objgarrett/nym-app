19459be0aeb2de7353d5b22e66eb37d44408ae42
unknown <shear_jbootcamp@hotmail.com>
2018-03-03 11:49:03 -0500

    im a dumb

--- config/connection.js
+++ config/connection.js
@@ -9,18 +9,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-<<<<<<< HEAD
         // password: process.env.DB_PASS,
         // database: 'nym_db'
         // // //database: 'NYM_DB'
         password: "password",
         database: 's2lugpg6wdxixion1'
-=======
-        password: process.env.DB_PASS,
-        database: 'nym_db'
-        // password: "password",
-        // database: 's2lugpg6wdxixion1'
->>>>>>> b4cbb63e0abd03959e832d456d1af036a4745d08
     });
 };    
 
