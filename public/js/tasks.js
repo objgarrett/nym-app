f58837d5a62becfee7a4928324a2f449a4975091
unknown <shear_jbootcamp@hotmail.com>
2018-03-02 22:46:52 -0500

    merge conflicts

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,5 +1,5 @@
 var facebook;
-
+var num = 0;
     // This is called with the results from from FB.getLoginStatus().
     //Facebook login validation. If valid, continues storing the facebook id as global var facebook
     //if invalid, reroute to login somehow. God help us. 
@@ -97,6 +97,7 @@ var mainFxn = () =>{
         }).done(apiTasks => {
           console.log(apiTasks);
           for (var j = 0; j < apiTasks.length; j++){
+            adder();
             if (parseInt(apiTasks[j].userid) === parseInt(facebook)) {
               var person
               var date = moment.unix(apiTasks[j].dueby).format("MM/DD/YYYY");
@@ -106,7 +107,7 @@ var mainFxn = () =>{
                   person = apiUsers[i].firstname + " " + apiUsers[i].lastname;
                 }
               }
-              var divToAdd = $(`<div class="div-to-append user-task" facebook="${facebook}" taskid="${apiTasks[i].taskid}"><div class="row"><div class="col-sm" id="taskcontainer"><div><img src="css/images/edit-white.png" alt="update button" id="check-button"></div><div id="tasks">${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
+              var divToAdd = $(`<div class="div-to-append user-task" facebook="${facebook}" taskid="${apiTasks[i].taskid}" id="${num}"><div class="row"><div class="col-sm" id="taskcontainer"><div><img src="css/images/edit-white.png" alt="update button" class="check-button" name="${num}"></div><div id="tasks">${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" class="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" class="delete-button"><i class="far fa-caret-square-down" class="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
               $("#task-append").append(divToAdd);
             } 
           }
@@ -119,7 +120,7 @@ var mainFxn = () =>{
                   person = apiUsers[i].firstname + " " + apiUsers[i].lastname;
                 }
               }
-              var divToAdd = $(`<div class="div-to-append user-task"><div class="row"><div class="col-sm" id="taskcontainer"><div id="tasks"><img src="css/images/edit-white.png" alt="update button" id="check-button">${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
+              var divToAdd = $(`<div class="div-to-append user-task"><div class="row"><div class="col-sm" id="taskcontainer"><div id="tasks"><img src="css/images/edit-white.png" alt="update button" class="check-button">${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" class="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" class="delete-button"><i class="far fa-caret-square-down" class="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
               $("#task-append").append(divToAdd);
             } 
           }
@@ -135,7 +136,8 @@ var mainFxn = () =>{
 
 
 $(".task-append").on("click", "img#check-button", () => {
-  console.log($("this"));
+  console.log($("img#check-button"));
+
 })
 
 // var divToAdd = $(<div class="div-to-append user-task"><div class="row"><div class="col-sm" id="taskcontainer"><div id="tasks"><i class="fas fa-check"></i>${apiTasks[i].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>)
@@ -149,4 +151,8 @@ documentHider();
 var documentShower = () => {
   $("#document").show();
   mainFxn();
+}
+
+var adder = () => {
+  num ++;
 }
