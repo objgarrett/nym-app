47c0ec8295cae3a83c4373ccb05d8a1de9e90eea
unknown <shear_jbootcamp@hotmail.com>
2018-03-02 21:50:01 -0500

    something someting something I'm sleepy

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -100,12 +100,13 @@ var mainFxn = () =>{
             if (parseInt(apiTasks[j].userid) === parseInt(facebook)) {
               var person
               var date = moment.unix(apiTasks[j].dueby).format("MM/DD/YYYY");
+
               for (var i = 0; i< apiUsers.length; i++) {
                 if (apiTasks[j].userid === apiUsers[i].facebook_id) {
                   person = apiUsers[i].firstname + " " + apiUsers[i].lastname;
                 }
               }
-              var divToAdd = $(`<div class="div-to-append user-task" facebook="${facebook}" taskid="${apiTasks[i].taskid}"><div class="row"><div class="col-sm" id="taskcontainer"><div id="tasks"><div><img src="css/images/edit-white.png" alt="update button" id="check-button"></div>${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
+              var divToAdd = $(`<div class="div-to-append user-task" facebook="${facebook}" taskid="${apiTasks[i].taskid}"><div class="row"><div class="col-sm" id="taskcontainer"><div><img src="css/images/edit-white.png" alt="update button" id="check-button"></div><div id="tasks">${apiTasks[j].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>`)
               $("#task-append").append(divToAdd);
             } 
           }
@@ -134,7 +135,7 @@ var mainFxn = () =>{
 
 
 $(".task-append").on("click", "img#check-button", () => {
-  console.log(this);
+  console.log($("this"));
 })
 
 // var divToAdd = $(<div class="div-to-append user-task"><div class="row"><div class="col-sm" id="taskcontainer"><div id="tasks"><i class="fas fa-check"></i>${apiTasks[i].text}</div><img src="css/images/edit-white.png" alt="update button" id="update-button"><img src="css/images/garbage-2-white.png" alt="delete button" id="delete-button"><i class="far fa-caret-square-down" id="more-button"></i></div></div><div class="row"><div class="col-sm"><p id="task-edit">Update Chore:</p><input type="text" id="task-edit"><p id="assigned-to">Assigned to: ${person}</p><input type="text" id="assigned-to"><p id="due-by">Due On: ${date}</p><input type="date" id="due-by"></div></div></div>)
