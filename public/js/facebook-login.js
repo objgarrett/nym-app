e7d39d472cce5f27c64d4296b492dbf93b9007ec
unknown <shear_jbootcamp@hotmail.com>
2018-03-03 02:43:18 -0500

    minor tweaks

--- public/js/facebook-login.js
+++ public/js/facebook-login.js
@@ -1,21 +1,14 @@
     // This is called with the results from from FB.getLoginStatus().
     function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
         // The response object is returned with a status field that lets the
         // app know the current login status of the person.
         // Full docs on the response object can be found in the documentation
         // for FB.getLoginStatus().
         if (response.status === 'connected') {
           // Logged into your app and Facebook.
-          console.log("Logged in by the powers vested in me by the state of virginia i do hereby pronounce you a beagle")
-          console.log(response.authResponse.userID)
-          //redirect needs to happen here
-          testAPI();            
           window.location.replace("/tasks");
 
         } else {
-            console.log("should go back to login");
             window.location.replace("/login")
             // The person is not logged into your app or we are unable to tell.
         }
@@ -63,8 +56,6 @@
       // Here we run a very simple test of the Graph API after login is
       // successful.  See statusChangeCallback() for when this call is made.
     function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
         FB.api('/me', function(response) {
-          console.log('Successful login for: ' + response.name);
         });
     }
