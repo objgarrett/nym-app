2bdeb28c3c284da537b72421689404aab9154149
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-03-02 20:56:41 -0500

    updates to styles and tasks

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,8 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-<<<<<<< HEAD
-        // password: "MC95901720",
         password: process.env.DB_PASS,
         database: 'nym_db'
-=======
-        password: "password",
-        database: 's2lugpg6wdxixion1'
->>>>>>> d817ba7f03c9ac812d0134b8fee995083bb9d12c
     });
 };    
 
